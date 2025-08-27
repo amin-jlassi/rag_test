e94384583868ca48e9c666cd363694607ba41419
amin <pcjlassi@gmail.com>
2025-08-28 00:59:53 +0100

    v5

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from openai import OpenAI
 from sentence_transformers import SentenceTransformer
 from pickle import load , dump
 from datetime import datetime
-
+from utils import exist , savePdf_inFile
 
 start = datetime.now()
 
@@ -24,14 +24,14 @@ class PdfLoader:
     def __init__(self , file_path):
         self.file_path = file_path
     
-    def exctract_text(self) : 
+    def extract_text(self) : 
         text = ""
         with pdfplumber.open(self.file_path) as pdf : 
             for page in pdf.pages : 
                 text += page.extract_text()
         return text
     
-    def exctract_tables(self) : 
+    def extract_tables(self) : 
         with pdfplumber.open(self.file_path) as pdf : 
             for page in pdf.pages : 
                 table = page.extract_tables()
@@ -73,13 +73,11 @@ class Chunker :
 class Embeddings : 
 
     def __init__(self  ):
+        self.model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
         self.embeddings = None
-    
     def generate_embeddings(self , chunks) : 
-        model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-        self.embeddings = model.encode(chunks)
-        
-
+        self.embeddings = self.model.encode(chunks)
+    
         return self.embeddings
     def save(self , index_path = "index.bin") :
 
@@ -116,7 +114,7 @@ class Embeddings :
 
 
     
-from utils import exist , savePdf_inFile
+
 
 folder_path = "dataset"
 with os.scandir(folder_path) as entries : 
@@ -136,21 +134,16 @@ with os.scandir(folder_path) as entries :
                 print("file already exists")
 
 index = Embeddings().get_index()
-model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-
 chunks = Chunker().load_chunks()
 
 
+query = "arithmetique"
+query_emb = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2').encode([query] , convert_to_numpy=True)
 
-
-
-query = "fichiers"
-query_emb = model.encode([query] , convert_to_numpy=True)
-
-distances , indices = index.search(query_emb , k=4)
+distances , indices = index.search(query_emb , k=1)
 retrieved_chunks = [chunks[i] for i in indices[0]]
-
-"""print(len(retrieved_chunks))
+"""
+print(len(retrieved_chunks))
 
 for x in retrieved_chunks : 
     print(x)
